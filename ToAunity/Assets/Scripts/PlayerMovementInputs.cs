4b740661ef5f0c05e4c6c2bcf8168f5a88edd5c0
Wendrul <dethomassin.etienne@gmail.com>
2020-03-11 17:33:47 +0100

    Initial upload of the project

---
+++ ToAunity/Assets/Scripts/PlayerMovementInputs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementInputs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private CharacterController2D character;
+    private bool hop;
+    private float move;
+
+    void Start()
+    {
+        character = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            hop = true;
+        }
+        move = Input.GetAxis("Horizontal");
+        character.Move(move, false, hop);
+        hop = false;
+    }
+}
